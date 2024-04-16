1fd8f7a6e625b94655fdb509d4ddb01921a308bc
Radosław Gryta <radek.gryta@gmail.com>
2024-04-17 00:18:31 +0200

    Push config

---
+++ run.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# For mounting storage from Asus Router:
+# mkdir /mnt/asus
+# sudo mount -t cifs -o username=test,password=test //192.168.1.1/Drive /mnt/asus
+
+docker-compose -f .\nginx_revproxy.yaml up -d
+docker-compose -f .\filebrowser.yaml up -d
+docker-compose -f .\pihole_unbound.yaml up -d
