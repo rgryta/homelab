19ab02e7338c79b9eb388d2f12f3e58280e399f1
Radosław Gryta <radek.gryta@gmail.com>
2024-04-18 22:43:35 +0200

    Add execute permissions

--- uninstall.sh
+++ uninstall.sh
@@ -4,7 +4,12 @@
 # mkdir /mnt/asus
 # sudo mount -t cifs -o username=test,password=test //192.168.1.1/Drive /mnt/asus
 
-docker-compose -f nginx_revproxy.yaml stop -v
-docker-compose -f filebrowser.yaml stop -v
-docker-compose -f pihole_unbound.yaml stop -v
-docker-compose -f jellyfin.yaml stop -v
+docker-compose -f nginx_revproxy.yaml stop
+docker-compose -f filebrowser.yaml stop
+docker-compose -f pihole_unbound.yaml stop
+docker-compose -f jellyfin.yaml stop
+
+docker-compose -f nginx_revproxy.yaml rm -v
+docker-compose -f filebrowser.yaml rm -v
+docker-compose -f pihole_unbound.yaml rm -v
+docker-compose -f jellyfin.yaml rm -v
