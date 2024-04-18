489e2319c85456aabcb2f207083608d187b303a7
Radosław Gryta <radek.gryta@gmail.com>
2024-04-18 22:21:24 +0200

    Update prepare.sh

--- prepare.sh
+++ prepare.sh
@@ -23,14 +23,14 @@ mkdir -p volumes/jellyfin
 
 # Update path
 PTH=$(printf '%s\n' "$(pwd)" | sed -e 's/[\/&]/\\&/g')
-sed -i 's/<path>/$PTH/g' *.yaml
+sed -i "s/<path>/$PTH/g" *.yaml
 
 # Update web password (e.g. for pihole web interface)
 PASS=$(printf '%s\n' "$WEB_PASS" | sed -e 's/[\/&]/\\&/g')
-sed -i 's/<password>/$PASS/g' *.yaml
+sed -i "s/<password>/$PASS/g" *.yaml
 
 # Update SAMBA config
 USR=$(printf '%s\n' "$SAMBA_USER" | sed -e 's/[\/&]/\\&/g')
-sed -i 's/<smb_user>/$USR/g' *.yaml
+sed -i "s/<smb_user>/$USR/g" *.yaml
 PASS=$(printf '%s\n' "$SAMBA_PASS" | sed -e 's/[\/&]/\\&/g')
-sed -i 's/<smb_pass>/$PASS/g' *.yaml
+sed -i "s/<smb_pass>/$PASS/g" *.yaml
