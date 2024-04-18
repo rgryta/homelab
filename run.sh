d9f64739d1f6fed65227cf6b5eff4f60044d7261
Radosław Gryta <radek.gryta@gmail.com>
2024-04-18 22:28:37 +0200

    Update run.sh

--- run.sh
+++ run.sh
@@ -4,7 +4,7 @@
 # mkdir /mnt/asus
 # sudo mount -t cifs -o username=test,password=test //192.168.1.1/Drive /mnt/asus
 
-docker-compose -f .\nginx_revproxy.yaml up -d
-docker-compose -f .\filebrowser.yaml up -d
-docker-compose -f .\pihole_unbound.yaml up -d
-docker-compose -f .\jellyfin.yaml up -d
+docker-compose -f nginx_revproxy.yaml up -d
+docker-compose -f filebrowser.yaml up -d
+docker-compose -f pihole_unbound.yaml up -d
+docker-compose -f jellyfin.yaml up -d
